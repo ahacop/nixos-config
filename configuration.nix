bb643a3032147d1d8564d38dcdea75f605a0cbab
Ara Hacopian <ara@hacopian.de>
2021-03-02 14:33:11 +0100

    Add config for postgres

--- configuration.nix
+++ configuration.nix
@@ -224,6 +224,13 @@
     postgresql = {
       enable = true;
       package = pkgs.postgresql_12;
+      authentication = lib.mkForce ''
+        # Generated file; do not edit!
+        # TYPE  DATABASE        USER            ADDRESS                 METHOD
+        local   all             all                                     trust
+        host    all             all             127.0.0.1/32            trust
+        host    all             all             ::1/128                 trust
+      '';
     };
 
     xserver = {
