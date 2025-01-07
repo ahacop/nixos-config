755de37c991062cde8c7929d3e758bfbdfe32ea4
Ara Hacopian <ara@hacopian.de>
2025-01-07 12:41:02 +0100

    Update flake

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -467,10 +467,11 @@
         trouble.enable = true;
         fidget = {
           enable = true;
-          progress = {
-            suppressOnInsert = true;
-            ignoreDoneAlready = true;
-            pollRate = 0.5;
+          settings = {
+            progress = {
+              suppress_on_insert = true;
+              ignore_done_already = true;
+            };
           };
         };
 
