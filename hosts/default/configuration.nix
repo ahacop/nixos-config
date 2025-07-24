0a1a26aa1c0797b901ec64ad31b0e2aadc672bb1
Ara Hacopian <ara@hacopian.de>
2025-07-24 17:19:39 +0200

    Add chromium

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -321,6 +321,7 @@ in {
       xxd
       yt-dlp
       zip
+      chromium
 
       # For hypervisors that support auto-resizing, this script forces it.
       # I've noticed not everyone listens to the udev events so this is a hack.
